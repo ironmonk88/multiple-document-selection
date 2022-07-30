1aea4f8088ba99f034ee954eda550999fa6b4030
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2022-07-30 16:51:39 -0700

    1.0.0 changes

--- multiple-document-ownership-config.js
+++ multiple-document-ownership-config.js
@@ -13,32 +13,44 @@ export const WithOwnershipConfig = (ConfigClass) => {
 
         getData(options) {
             // User permission levels
-            const playerLevels = Object.entries(CONST.DOCUMENT_META_OWNERSHIP_LEVELS).map(([name, level]) => {
-                return { level, label: game.i18n.localize(`OWNERSHIP.${name}`) };
-            });
-
-            for (let [name, level] of Object.entries(CONST.DOCUMENT_OWNERSHIP_LEVELS)) {
-                if ((level < 0)) continue;
-                playerLevels.push({ level, label: game.i18n.localize(`OWNERSHIP.${name}`) });
+            let playerLevels = {};
+            if (isNewerVersion(game.version, "9.999999")) {
+                playerLevels = Object.entries(CONST.DOCUMENT_META_OWNERSHIP_LEVELS).map(([name, level]) => {
+                    return { level, label: game.i18n.localize(`OWNERSHIP.${name}`) };
+                });
+                for (let [name, level] of Object.entries(CONST.DOCUMENT_OWNERSHIP_LEVELS)) {
+                    if ((level < 0)) continue;
+                    playerLevels.push({ level, label: game.i18n.localize(`OWNERSHIP.${name}`) });
+                }
+            } else {
+                playerLevels["-2"] = game.i18n.localize("PERMISSION.DEFAULT");
+                playerLevels["-1"] = game.i18n.localize("PERMISSION.NOCHANGE");
+                for (let [n, l] of Object.entries(CONST.DOCUMENT_PERMISSION_LEVELS)) {
+                    playerLevels[l] = game.i18n.localize(`PERMISSION.${n}`);
+                }
             }
 
             // Default permission levels
             const defaultLevels = foundry.utils.deepClone(playerLevels);
-            defaultLevels.shift();
+            if (isNewerVersion(game.version, "9.999999")) {
+                defaultLevels.shift();
+            } else {
+                delete defaultLevels["-2"];
+            }
 
             // Player users
             const users = game.users.map(user => {
                 return {
                     user,
-                    level: CONST.DOCUMENT_META_OWNERSHIP_LEVELS.NOCHANGE,
+                    level: CONST.DOCUMENT_META_OWNERSHIP_LEVELS?.NOCHANGE ?? "-1",
                     isAuthor: false
                 };
             });
 
             // Construct and return the data object
             return {
-                currentDefault: CONST.DOCUMENT_META_OWNERSHIP_LEVELS.NOCHANGE,
-                instructions: game.i18n.localize("OWNERSHIP.HintDocument"),
+                currentDefault: CONST.DOCUMENT_META_OWNERSHIP_LEVELS?.NOCHANGE ?? "-1",
+                instructions: game.i18n.localize(isNewerVersion(game.version, "9.999999") ? "OWNERSHIP.HintDocument" : "PERMISSION.HintDocument"),
                 defaultLevels,
                 playerLevels,
                 isFolder: false,
@@ -50,7 +62,7 @@ export const WithOwnershipConfig = (ConfigClass) => {
             event.preventDefault();
             if (!game.user.isGM) throw new Error("You do not have the ability to configure permissions.");
 
-            const metaLevels = CONST.DOCUMENT_META_OWNERSHIP_LEVELS;
+            const metaLevels = isNewerVersion(game.version, "9.999999") ? CONST.DOCUMENT_META_OWNERSHIP_LEVELS : { DEFAULT: -2, NOCHANGE: -1 };
             const omit = metaLevels.NOCHANGE;
             const ownershipLevels = {};
             for (let [user, level] of Object.entries(formData)) {
@@ -63,12 +75,17 @@ export const WithOwnershipConfig = (ConfigClass) => {
 
             let cls = getDocumentClass(this.documents[0].documentName);
             const updates = this.documents.map(d => {
-                const ownership = foundry.utils.deepClone(d.ownership);
+                const ownership = foundry.utils.deepClone(isNewerVersion(game.version, "9.999999") ? d.ownership : d.data.permission);
                 for (let [k, v] of Object.entries(ownershipLevels)) {
                     if (v === metaLevels.DEFAULT) delete ownership[k];
                     else ownership[k] = v;
                 }
-                return { _id: d.id, ownership };
+                let data = { _id: d.id };
+                if (isNewerVersion(game.version, "9.999999"))
+                    data.ownership = ownership;
+                else
+                    data.permission = ownership;
+                return data;
             });
 
             return cls.updateDocuments(updates, { diff: false, recursive: false, noHook: true });
